7b58afa4541d578955677816d188b14de3125414
SandeepDesu <sandeep.desu2@t-mobile.com>
2021-10-19 17:55:51 +0530

    nx project setup

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BookComponent } from './component/book/book.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SidenavComponent } from './component/sidenav/sidenav.component';
+import { SpinnerComponent } from './component/spinner/spinner.component';
+import { MorePipe } from './pipes/more.pipe';
+import { PhonePipe } from './pipes/phone.pipe';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    BookComponent,
+    HeaderComponent,
+    SidenavComponent,
+    SpinnerComponent,
+    MorePipe,
+    PhonePipe
+  ],
 })
 export class SharedModule {}
